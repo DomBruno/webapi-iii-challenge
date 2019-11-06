8d709b0251775050aff6412cd992217c584a3c69
Dominick Bruno <dbruno@unseen.is>
2019-11-06 09:02:31 -0500

    Frameworks set up, most of postRouter written

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,71 @@
 const express = 'express';
-
+const postDB = require('./postDB');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-
+    postDB.get()
+    .then(posts => {
+        res
+        .status(200)
+        .json(posts);
+    })
+    .catch(err => {
+        res
+        .status(500)
+        .json({
+            message: "Error retrieving posts data", err
+        });
+    });
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', validatePostId, (req, res) => {
+        res
+        .status(200)
+        .json(req.post);
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validatePostId, (req, res) => {
+        const deletedPost = db.getById(id);
+        postDB.remove(id)
+        .then(() => {
+            res
+            .stats(204)
+            .json({
+                message: `Removed ${deletedPost}`
+            })
+        })
+        .catch(err => {
+            res
+            .status(500)
+            .json({
+                message: 'Error removing post.', err
+            });
+        });
+    });
 
-});
-
-router.put('/:id', (req, res) => {
+router.put('/:id', validatePostId, (req, res) => {
 
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
-
+    const { id } = req.params;
+    postDB.getById(id)
+      .then(post => {
+        post
+          ? ((req.post = post), next())
+          : res
+              .status(404)
+              .json({
+                   message: `Post with id ${id} could not be found` });
+      })
+      .catch(err => {
+        res
+        .status(500)
+        .json({
+             message: "Error retrieving posts data", err });
+      });
 };
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,8 +7,21 @@ server.get('/', (req, res) => {
 });
 
 //custom middleware
-
 function logger(req, res, next) {
+  const date = Date.now();
+  console.log(`${req.method} Request : ${req.url} URL : ${date}`);
+  next();
+};
+
+function validateUserId(req, res, next) {
+  const id = req.params.id;
+};
+
+function validateUser(req, res, next) {
+  req.body = req.body || 
+};
+
+function validatePost(req, res, next) {
 
 };
 
