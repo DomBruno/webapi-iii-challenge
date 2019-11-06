1d787e983d030f420f0167fc330c222ce915a6ac
Dominick Bruno <dbruno@unseen.is>
2019-11-06 10:29:03 -0500

    completed user GET endpoint frameworks

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,5 +1,7 @@
 const express = 'express';
 const userDB = require('./userDb');
+const postDB = require('../posts/postDb');
+
 const router = express.Router();
 
 router.post('/', validateUser, (req, res) => {
@@ -11,15 +13,50 @@ router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
 });
 
 router.get('/', (req, res) => {
-
+    userDB.get()
+    .then(users => {
+      res
+      .status(200)
+      .json(users);
+    })
+    .catch(err => {
+      res
+      .status(500)
+      .json({ 
+          message: "Error retrieving User data", err
+         });
+    });
 });
 
 router.get('/:id', validateUserId, (req, res) => {
-
+    userDB.getById(req.params.id)
+    .then(user => {
+      res
+      .status(200)
+      .json(user);
+    })
+    .catch(err => {
+      res
+      .status(500)
+      .json({ 
+          message: "Error retrieving user", err 
+        });
+    });
 });
 
 router.get('/:id/posts', validateUserId, (req, res) => {
-
+    userDB.getUserPosts(req.params.id)
+    .then(posts => {
+      res
+      .status(200
+        .json(posts);
+    })
+    .catch(err => {
+      res
+      .status(500)
+      .json({ message: "Error retrieving user posts", err
+     });
+    });
 });
 
 router.delete('/:id', validateUserId, (req, res) => {
